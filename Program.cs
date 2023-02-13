880d7ca4af9271ce109fe7e46f843635c71d0608
Ilya <pulinilija@gmail.com>
2023-02-13 12:46:22 +0700

    А

--- Program.cs
+++ Program.cs
@@ -1,24 +1,6 @@
-﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-2, 3, 7 -> 7
-44 5 78 -> 78
-22 3 9 -> 22 */
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
 
-int number;
-Console.Write("Введите первое число: ");
-int.TryParse(Console.ReadLine()!, out number);
-
-int number2;
-Console.Write("Введите второе число: ");
-int.TryParse(Console.ReadLine()!, out number2);
-
-int number3;
-Console.Write("Введите третье число: ");
-int.TryParse(Console.ReadLine()!, out number3);
-
-if (number > number2 && number > number3)
-    Console.Write($"Максимальное число - {number}");
-
-else if (number2 > number && number2 > number3)
-    Console.Write($"Максимальное число - {number2}");
-else
-    Console.Write($"Максимальное число - {number3}");
