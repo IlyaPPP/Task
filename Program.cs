65af8821bb07c6f6948ddb97935f97aa46b095c4
Ilya <pulinilija@gmail.com>
2023-02-13 13:29:13 +0700

    Задача 4 н

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿#pragma warning disable format
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
 
-#pragma warning restore format
+int N;
+Console.Write("Введите число: ");
+bool flag = int.TryParse(Console.ReadLine()!, out N);
+if (flag)
+{
+    if (N > 0)
+    {
+        for (int i2 = 2; i2 <= N; i2 += 2)
+            Console.Write($" {i2} ");
+    }
+    else
+    {
+        for (int i2 = -2; i2 >= N; i2 -= 2)
+            Console.Write($" {i2} ");
+    }
+}
+else
+    Console.Write("Неверный ввод");
