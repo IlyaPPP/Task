d01d17a71bab8ca464a65c7ab6b572605f4196c2
Ilya <pulinilija@gmail.com>
2023-02-13 13:17:07 +0700

    Задача 4

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
-﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
-является ли число чётным (делится ли оно на два без остатка).
-4 -> да
--3 -> нет
-7 -> нет*/
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
 
-int number;
+int N;
 Console.Write("Введите число: ");
-int.TryParse(Console.ReadLine()!, out number);
-
-if (number % 2 == 0)
-    Console.Write("Число является четным");
+bool flag = int.TryParse(Console.ReadLine()!, out N);
+if (flag)
+{
+    if (N > 0)
+    {
+        for (int i2 = 2; i2 <= N; i2 += 2)
+            Console.Write($" {i2} ");
+    }
+    else
+    {
+        for (int i2 = -2; i2 >= N; i2 -= 2)
+            Console.Write($" {i2} ");
+    }
+}
 else
-    Console.Write("Число является нечетным");
+    Console.Write("Неверный ввод");
